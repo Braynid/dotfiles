421fe46bb7b006463c703643b6f2889130234cc7
Bogdan Popa <geraldb.popa@gmail.com>
2016-09-02 13:43:05 +0300

    Add aliases for showing / hiding hidden files

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,2 +1,5 @@
 #alias pbcopy='xclip -selection clipboard'
 #alias pbpaste='xclip -selection clipboard -o'
+
+alias showHiddenF='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideHiddenF='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
